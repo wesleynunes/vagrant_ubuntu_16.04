e8348907951f98c8383ecc0d471c67992f7413f9
Wesley Nunes <wesleysilva.ti@gmail.com>
2019-03-21 16:48:03 -0300

    correcao na instalacao php

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,12 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
     config.vm.hostname = "xenial"
-    config.vm.network :private_network, ip: "192.168.33.70" 
+    config.vm.network :private_network, ip: "192.168.33.78" 
     config.vm.synced_folder("./", "/var/www/html", :nfs => true)
     config.vm.provider "virtualbox" do |machine|
-    	machine.memory = 1024
-        machine.cpus = 2
-    	machine.name = "xenial"
+      machine.memory = 2024
+      machine.cpus = 2
+      machine.name = "xenial"
     end
     config.vm.provision :shell, path: "setup.sh"
 end
--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
-#/usr/bin/env bash
-
+# Instalando e configurando pacotes php 
 echo "--- Instalando pacotes para desenvolvimento [PHP]"
 
-# Use single quotes instead of double quotes to make it work with special-character passwords
-echo "--- definir senha e nome do projeto ---"
+# Criar variaves para senha mysql, nome do projeto, git
+echo "--- Definir senha mysql, nome do projeto, nome email do git---"
 PASSWORD='123'
 PROJECTFOLDER='project'
+GITNAME="Wes"
+GITEMAIL="wesleysilva.ti@gmail.com"
 
 echo "--- criar pasta do projeto ---"
 sudo mkdir "/var/www/html/${PROJECTFOLDER}"
@@ -16,21 +17,23 @@ echo "--- update / upgrade ---"
 sudo apt-get update
 sudo apt-get -y upgrade
 
-echo "--- instalar apache2 and php5 ---"
+echo "--- instalar apache2 ---"
 sudo apt-get -y install apache2
+
+echo "--- instalar php, libapache2-mod-php ---"
 sudo apt-get -y install php libapache2-mod-php
 
-echo "--- instalar cURL and Mcrypt ---"
+echo "--- instalar modulos php ---"
 sudo apt-get -y install php-curl
 sudo apt-get -y install php-mcrypt
 
-echo "--- instalar mysql e fornercer senha para o instalador -- "
+echo "--- instalar mysql e fornercer senha para o instalador, instalando php-mysql-- "
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get -y install mysql-server
 sudo apt-get -y install php-mysql
 
-echo "--- instalar mysql e fornecer senha para o instalador"
+echo "--- instalar phpmyadmin e fornecer senha para o instalador"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
@@ -38,7 +41,7 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get -y install phpmyadmin
 
-echo "--- arquivo host ---"
+echo "--- Configurando arquivo host ---"
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
     DocumentRoot "/var/www/html/${PROJECTFOLDER}"
@@ -54,19 +57,22 @@ echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 echo "--- habilitar mod-rewrite do apache ---"
 sudo a2enmod rewrite
 
+echo "--- reiniciar mysql ---"
+sudo service mysql restart
+
 echo "--- reiniciar apache ---"
 sudo service apache2 restart
 
-echo "--- reiniciar apache ---"
-sudo service mysql restart
-
-echo "--- instalando git ---"
+echo "--- instalando e configurando git ---"
 sudo apt-get -y install git 
-git config --global user.name "seunome"  
-git config --global user.email seuemail@.com.br
+git config --global user.name ${GITNAME} 
+git config --global user.email ${GITEMAIL}
 
 echo "-- instalar composer"
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+
+echo "--- reiniciar apache ---"
+sudo service apache2 restart
 
 echo "[OK] --- Ambiente de desenvolvimento concluido ---"
